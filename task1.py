86841c29b469a1acf54a6e119f378a1e39ed3c77
AkoToSiJeromeEh <jevi.gaspar.au@phinmaed.com>
2023-06-29 21:46:27 +0800

    assignment

---
+++ task1.py
@@ -0,0 +1,5 @@
+
+num1 = 3.5
+num2 = 4.6
+sum = num1 + num2
+print("The sum is", format(sum))
---
+++ task2.py
@@ -0,0 +1,12 @@
+num1 = 5
+num2 = 3
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("The sum is", format(sum))
+print("The difference is", format(difference))
+print("The product is", format(product))
+print("The quotient is", format(quotient))
