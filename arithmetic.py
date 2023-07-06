b69db124923dc4c2b1eef1f10d7ec8fc09501a71
Jeje <114987334+AkoToSiJeromeEh@users.noreply.github.com>
2023-07-06 13:42:13 +0800

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+num1 = 5 
+num2 = 8
+
+sumOfNums = num1 + num2
+differenceOfNums = num1 - num2
+productOfNums = num1 * num2
+quotientOfNums = num1 / num2
+
+print(
+      "Sum : {0}".format(sumOfNums),  
+      "Difference : {0}".format(differenceOfNums), 
+      "Product : {0}".format(productOfNums),
+      "Quotient : {0}".format(quotientOfNums)
+)
---
+++ print-act.py
@@ -0,0 +1,34 @@
+
+Programmer = "Jerome "
+DateToday = "July 6 2023"
+Description = 'Personal Information'
+Fname = 'Jerome Gabriel'
+Mname = 'Villavicencio'
+Lname = "Gaspar"
+Course = 'Bsit'
+Age = 19
+Gender = "Male"
+CivilStats = "Taken"
+Nationality = "Filipino"
+Motto = "Nothing is Impossible"
+School = "Phinma Aurollo University"
+College = "Cite"
+print('BIO DATA')
+print('Programmer : ' , Programmer)
+print ('Date Created : ' , DateToday)
+print ('Description of the Program : ' , Description)
+print('--------------------------------------------')
+print ('First Name : ' , Fname )
+print ('Middle Name : ' , Mname)
+print ('Last Name : ' , Lname)
+print ('Course : ' , Course)
+print ('Age : ' , Age)
+print ('Gender : ' , Gender)
+print ('Civil Status :  ' , CivilStats)
+print ('Nationality : ' , Nationality)
+print ('Motto : ' , Motto)
+print ('School : ' , School)
+print ('College : ' , College)
+
+
+
